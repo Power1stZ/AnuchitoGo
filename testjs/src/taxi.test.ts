9feff98372cbc4c7c2b48a3140d3190da3c72e0c
Nattapong Kueakool <nattapong.k@arise.tech>
2024-05-15 10:55:08 +0700

    First Commit

--- testjs/src/taxi.test.ts
+++ testjs/src/taxi.test.ts
@@ -1,8 +1,30 @@
 import {Taxi} from './taxi'
 
 describe('Taxi', () => {
-  it('example test', () => {
-    const result = Taxi()
-    expect(result).toEqual(55)
+  it('(int)KM and (int)TIME', () => {
+    const result = Taxi(10,10)
+    expect(result).toEqual(50)
   })
+
+  it('(int)KM and (float)TIME > x.1',()=>{
+    const result = Taxi(10,10.5)
+    expect(result).toEqual(51)
+  })
+
+  it('x.5 > (float)KM > x.1 and (int)TIME',()=>{
+    const result = Taxi(10.2,10)
+    expect(result).toEqual(52)
+  })
+
+  it('x.5 < (float)KM < x.1 and (int)TIME',()=>{
+    const result = Taxi(10.8,10)
+    expect(result).toEqual(54)
+  })
+
+  it('x.5 == (float)KM and (int)TIME',()=>{
+    const result = Taxi(10.5,10)
+    expect(result).toEqual(52)
+  })
+
+
 })
--- testjs/src/taxi.ts
+++ testjs/src/taxi.ts
@@ -1,1 +1,6 @@
-export const Taxi = () => 55
+export const Taxi = (distance:number,time:number) => {
+    const cDistance:number = (Math.ceil(distance / 0.5) * 0.5)*4
+    const cTime:number = Math.ceil(time)*1
+    console.log(cDistance,cTime)
+    return cDistance + cTime
+}
