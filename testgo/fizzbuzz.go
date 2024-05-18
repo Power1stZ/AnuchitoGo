5257692f440ffddfd801b57d2f49531a7db10b8d
Power1stZ <nattapong1999.dev@gmail.com>
2024-05-18 20:10:22 +0700

    TestFizzBuzzShouldReturn1WhenInput1

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -1,28 +1,5 @@
 package main
 
-import "strconv"
-
 func FizzBuzz(n int) string {
-	if IsFizzBuzz(n) {
-		return "FizzBuzz"
-	}
-	if IsBuzz(n) {
-		return "Buzz"
-	}
-	if IsFizz(n) {
-		return "Fizz"
-	}
-	return strconv.Itoa(n)
-}
-
-func IsFizz(n int) bool {
-	return n%3 == 0
-}
-
-func IsBuzz(n int) bool {
-	return n%5 == 0
-}
-
-func IsFizzBuzz(n int) bool {
-	return n%15 == 0
+	return "1"
 }
