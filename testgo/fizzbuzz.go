540ee779e6e7111f82e49bf3acab652859a1847e
Power1stZ <nattapong1999.dev@gmail.com>
2024-05-19 16:16:12 +0700

    TestFizzBuzzShouldReturnBuzzWhenInput5

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -7,9 +7,17 @@ func FizzBuzz(n int) string {
 		return "Fizz"
 	}
 
+	if IsBuzz(n) {
+		return "Buzz"
+	}
+
 	return strconv.Itoa(n)
 }
 
 func IsFizz(n int) bool {
 	return n%3 == 0
+}
+
+func IsBuzz(n int) bool {
+	return n%5 == 0
 }
