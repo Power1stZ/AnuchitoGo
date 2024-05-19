4e3915c99ec74e754cec604f26429f5a6915f153
Power1stZ <nattapong1999.dev@gmail.com>
2024-05-19 16:09:13 +0700

    TestFizzBuzzShouldReturn1WhenInput1

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -1,18 +1,5 @@
 package main
 
-import "strconv"
-
 func FizzBuzz(n int) string {
-	return MappingFizzBuzz(n)
-}
-
-func MappingFizzBuzz(n int) string {
-	fizz := n%3 == 0
-	buzz := n%5 == 0
-
-	result := map[bool]string{
-		true:  "Fizz" + map[bool]string{true: "Buzz", false: ""}[buzz],
-		false: map[bool]string{true: "Buzz", false: strconv.Itoa(n)}[buzz],
-	}[fizz]
-	return result
+	return "1"
 }
